330b244ce9ea84e75c5da5bdb08ca4e3a216da05
André Restivo <andre.restivo@gmail.com>
2023-01-03 13:49:00 +0000

    feat: implemented day 2

--- day2/src/bin/day2a.rs
+++ day2/src/bin/day2a.rs
@@ -1,4 +1,20 @@
 fn main() {
   let lines = global::read_lines();
-  println!("{:?}", lines);
+
+  let mut current = 5;
+
+  for line in lines {
+    for m in line.chars() {
+      current = match m {
+        'U' => { if current < 4 { current } else { current - 3} },
+        'R' => { if current % 3 == 0 { current } else { current + 1 } },
+        'D' => { if current > 6 { current } else { current + 3} },
+        'L' => { if (current + 2) % 3 == 0 { current } else { current - 1} },
+        _ => unreachable!()
+      };
+    }  
+    print!("{}", current);
+  }
+
+  println!();
 }
--- day2/src/bin/day2b.rs
+++ day2/src/bin/day2b.rs
@@ -1,4 +1,34 @@
 fn main() {
   let lines = global::read_lines();
-  println!("{:?}", lines);
+
+  let keyboard = vec![
+    vec![' ', ' ', '1', ' ', ' '],
+    vec![' ', '2', '3', '4', ' '],
+    vec!['5', '6', '7', '8', '9'],
+    vec![' ', 'A', 'B', 'C', ' '],
+    vec![' ', ' ', 'D', ' ', ' '],
+  ];
+
+  let mut row = 2; let mut col = 0;
+
+  for line in lines {
+    for m in line.chars() {
+      let next_row = match m {
+        'U' => { if row == 0 { row } else { row - 1 } },
+        'D' => { if row == keyboard.len() - 1 { row } else { row + 1 } },
+        _ => { row }
+      };
+
+      let next_col = match m {
+        'L' => { if col == 0 { col } else { col - 1 } },
+        'R' => { if col == keyboard[0].len() - 1 { col } else { col + 1 } },
+        _ => { col }
+      };    
+      
+      (row, col) = if keyboard[next_row][next_col] == ' ' {(row, col)} else {(next_row, next_col)};
+    }  
+    print!("{}", keyboard[row][col]);
+  }
+
+  println!();
 }

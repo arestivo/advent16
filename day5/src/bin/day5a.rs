49f7391d69450d22803fa61e8d4d264ec741ca69
André Restivo <andre.restivo@gmail.com>
2023-01-04 10:46:40 +0000

    feat: implemented day 5

--- day5/src/bin/day5a.rs
+++ day5/src/bin/day5a.rs
@@ -1,4 +1,19 @@
 fn main() {
-  let lines = global::read_lines();
-  println!("{:?}", lines);
+  let line = global::read_line();
+
+  let mut i = 0;
+  let mut pwd = "".to_string();
+
+  loop {
+    let s = format!("{}{}", line, i);
+    let hash = format!("{:?}", md5::compute(s));
+    if hash.starts_with("00000") {
+      pwd.push(hash.chars().nth(5).unwrap());
+      if pwd.len() == 8 { break; }
+    }
+
+    i += 1;
+  }
+
+  println!("{}", pwd);
 }
--- day5/src/bin/day5b.rs
+++ day5/src/bin/day5b.rs
@@ -1,4 +1,26 @@
 fn main() {
-  let lines = global::read_lines();
-  println!("{:?}", lines);
+  let line = global::read_line();
+
+  let mut i = 0;
+  let mut pwd = "________".to_string();
+
+  loop {
+    let s = format!("{}{}", line, i);
+    let hash = format!("{:?}", md5::compute(s));
+    if hash.starts_with("00000") {
+      let pos = hash.chars().nth(5).unwrap();
+      if pos.is_numeric() {
+        let pos = pos.to_string().parse::<usize>().unwrap();
+        if pos < 8 && pwd.chars().nth(pos).unwrap() == '_' {
+          let c = hash.chars().nth(6).unwrap().to_string();
+          pwd.replace_range(pos..pos+1, c.as_str());
+        }
+      }
+      if !pwd.contains('_') { break; }
+    }
+
+    i += 1;
+  }
+
+  println!("{}", pwd);
 }

f68b7d916572352534817e50f603fee8d5057f0d
André Restivo <andre.restivo@gmail.com>
2023-01-04 11:04:30 +0000

    feat: implented day 6

--- day6/src/bin/day6a.rs
+++ day6/src/bin/day6a.rs
@@ -1,4 +1,26 @@
+use std::collections::HashMap;
+
 fn main() {
   let lines = global::read_lines();
-  println!("{:?}", lines);
+
+  let mut counts: [HashMap<char, i32>; 8] = [();8].map(|_| HashMap::new());
+
+  for line in &lines {
+    for (i, c) in line.char_indices() {
+      match counts[i].entry(c) {
+          std::collections::hash_map::Entry::Occupied(mut e) => { e.insert(e.get() + 1); }
+          std::collections::hash_map::Entry::Vacant(e) => { e.insert(1); }
+      }
+    }
+  }
+
+  let mut data = "".to_string();
+
+  (0..lines[0].len()).for_each(|i| {
+    let mut pairs: Vec<(char, i32)> = counts[i].iter().map(|(a, b)| (*a,*b)).collect();
+    pairs.sort_unstable_by_key(|p| (i32::MAX - p.1, p.0));
+    data.push(pairs[0].0);    
+  });
+
+  println!("{}", data);
 }

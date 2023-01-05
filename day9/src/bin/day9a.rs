0d21fdd34dd8ba1a33314f4869e071c7c0d13ac0
André Restivo <andre.restivo@gmail.com>
2023-01-05 20:40:54 +0000

    feat: implented day 9

--- day9/src/bin/day9a.rs
+++ day9/src/bin/day9a.rs
@@ -1,4 +1,43 @@
 fn main() {
-  let lines = global::read_lines();
-  println!("{:?}", lines);
+  let encoded = global::read_line();
+  let decoded = decode(&encoded);
+  println!("{}", decoded.len());
+}
+
+pub fn decode(encoded: &String) -> String {
+  let mut ptr = 0;
+  let mut inside_parens = false;
+  let mut marker = "".to_string();
+  let mut decoded = "".to_string();
+
+  while ptr < encoded.len() {
+    let c = encoded.chars().nth(ptr).unwrap();
+    if inside_parens {
+      if c == ')' {
+        let parts: Vec<&str> = marker.split('x').collect();
+        let size = parts[0].parse::<usize>().unwrap();
+        let times = parts[1].parse::<u32>().unwrap();
+
+        for _ in 0..times {
+          decoded.push_str(&encoded[ptr + 1..ptr + 1 + size]);
+        }
+
+        inside_parens = false;
+        marker = "".to_string();
+        ptr += size;
+      } else {
+        marker.push(c);
+      }
+      ptr += 1
+    } else {      
+      if c == '(' {
+        inside_parens = true;
+      } else {
+        decoded.push(c);
+      }
+      ptr += 1;
+    }
+  }
+
+  decoded
 }
--- day9/src/bin/day9b.rs
+++ day9/src/bin/day9b.rs
@@ -1,4 +1,41 @@
 fn main() {
-  let lines = global::read_lines();
-  println!("{:?}", lines);
+  let encoded = global::read_line();
+  let decoded = decode(&encoded);
+  println!("{}", decoded);
+}
+
+pub fn decode(encoded: &String) -> u64 {
+  let mut ptr = 0;
+  let mut inside_parens = false;
+  let mut marker = "".to_string();
+  let mut decoded = 0_u64;
+
+  while ptr < encoded.len() {
+    let c = encoded.chars().nth(ptr).unwrap();
+    if inside_parens {
+      if c == ')' {
+        let parts: Vec<&str> = marker.split('x').collect();
+        let size = parts[0].parse::<usize>().unwrap();
+        let times = parts[1].parse::<u64>().unwrap();
+
+        decoded += times * decode(&encoded[ptr + 1..ptr + 1 + size].to_string());
+
+        inside_parens = false;
+        marker = "".to_string();
+        ptr += size;
+      } else {
+        marker.push(c);
+      }
+      ptr += 1
+    } else {      
+      if c == '(' {
+        inside_parens = true;
+      } else {
+        decoded += 1;
+      }
+      ptr += 1;
+    }
+  }
+
+  decoded
 }

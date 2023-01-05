c1bde38875dc67306eab005d3212fdbee091231d
André Restivo <andre.restivo@gmail.com>
2023-01-05 09:31:24 +0000

    feat: implemented day 7

--- day7/src/bin/day7a.rs
+++ day7/src/bin/day7a.rs
@@ -1,4 +1,34 @@
+use fancy_regex::Regex;
+
 fn main() {
   let lines = global::read_lines();
-  println!("{:?}", lines);
+
+  let hypernet_regex = Regex::new(r"\[.*?\]").unwrap();
+  let abba_regex = Regex::new(r"(\w)(\w)\2\1").unwrap();
+
+  let count = lines.into_iter().filter(|address| {
+    let hypernets = hypernet_regex.captures_iter(address);
+
+    for hn in hypernets {
+      let hn = hn.unwrap().get(0).unwrap().as_str().to_string();
+      if contains_abba(&hn, &abba_regex) { return false; }
+    }
+
+    let address = hypernet_regex.replace_all(address, " ").to_string();
+    contains_abba(&address, &abba_regex)
+  }).count();
+
+  println!("{}", count);
+}
+
+fn contains_abba(s: &str, regex: &Regex) -> bool {
+  for candidate in regex.captures_iter(s) {
+    let candidate = candidate.unwrap();
+    let l1 = candidate.get(1).unwrap().as_str().to_string();
+    let l2 = candidate.get(2).unwrap().as_str().to_string();
+    
+    if  l1 != l2 { return true }
+  }
+
+  false
 }
--- day7/src/bin/day7b.rs
+++ day7/src/bin/day7b.rs
@@ -1,4 +1,43 @@
 fn main() {
   let lines = global::read_lines();
-  println!("{:?}", lines);
+
+  let mut count = 0;
+
+  for address in lines {
+    let parts: Vec<String> = address.split(&['[', ']'][..]).map(|s| s.to_string()).collect();
+
+    let mut supernets = vec![];
+    let mut hypernets = vec![];
+
+    for (i, p) in parts.iter().enumerate() {
+      if i % 2 == 0 { supernets.push(p.clone()); }
+      if i % 2 == 1 { hypernets.push(p.clone()); }
+    }
+
+    if supports_ssl(&supernets, &hypernets) { count += 1; }
+  }
+  
+  println!("{}", count);
+}
+
+fn supports_ssl(supernets: &Vec<String>, hypernets: &Vec<String>) -> bool {
+  for sn in supernets {
+    for hn in hypernets {
+      if contains_aba_bab(sn, hn) { return true }
+    }
+  }
+
+  false
+}
+
+fn contains_aba_bab(sn: &str, hn: &str) -> bool {
+  for i in 0..sn.len() - 2 {
+    let l1 = sn.chars().nth(i).unwrap();
+    let l2 = sn.chars().nth(i + 1).unwrap();
+    let l3 = sn.chars().nth(i + 2).unwrap();
+
+    if l1 == l3 && l1 != l2 && hn.contains(&format!("{}{}{}", l2, l1, l2)) { return true; }
+  }
+
+  false
 }
